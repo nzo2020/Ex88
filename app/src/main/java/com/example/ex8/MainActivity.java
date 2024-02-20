4d6ca51d62fdbae7ff65532cde106dad3aa9771e
nzo2020 <nz2020@bs.amalnet.k12.il>
2024-02-20 23:47:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex8/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.ex8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.webkit.WebSettings;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webV;
+    EditText eText;
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webV = (WebView) findViewById(R.id.webV);
+        eText = findViewById(R.id.eText);
+        WebSettings webSettings = webV.getSettings();
+        webV.setWebViewClient(new MyWebViewClient());
+
+        btn = findViewById(R.id.btn);
+
+
+
+    }
+
+    public void go(View view) {
+        String st = eText.getText().toString();
+        webV.loadUrl(st);
+
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+
+        }
+    }
+}
